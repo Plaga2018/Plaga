0a4263c7de1babe8de71e3506ebc5394145b04e2
Plaga-Devs <ContactPlaga2018@gmail.com>
2018-04-11 15:42:29 -0500

    setup wsgi
    setup wsgi.

--- learning_users/learning_users/wsgi.py
+++ learning_users/learning_users/wsgi.py
@@ -6,11 +6,26 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
+#
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_users.settings")
+#
+# application = get_wsgi_application()
 import os
+import sys
 
+# # add your project directory to the sys.path
+project_home = u'/home/lemayr1/Plaga/learning_users/'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'learning_users/learning_users.settings'
+
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_users.settings")
-
 application = get_wsgi_application()
