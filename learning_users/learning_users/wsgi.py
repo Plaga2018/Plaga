de9716fd352f95de62d97f6af0bf57c6de2a9b20
Plaga-Devs <ContactPlaga2018@gmail.com>
2018-04-11 21:44:05 -0500

    learning_users

--- learning_users/learning_users/wsgi.py
+++ learning_users/learning_users/wsgi.py
@@ -18,6 +18,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_users")
 
 application = get_wsgi_application()
