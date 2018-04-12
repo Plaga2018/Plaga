55e20b24a7b52b44a71ad41e0cba2ae716e9bc46
Plaga-Devs <ContactPlaga2018@gmail.com>
2018-04-11 20:49:37 -0500

    ran the collectstatic function

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = [STATIC_DIR,]
 
 # MEDIA INFORMATION:
 MEDIA_ROOT = MEDIA_DIR
